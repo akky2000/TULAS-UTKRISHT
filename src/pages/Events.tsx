38e0a273d503c851c80fd386896110b538ece044
dexoped <118817544+dexoped@users.noreply.github.com>
2025-11-11 11:36:06 +0530

    Poster changes

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -4,12 +4,14 @@ import Footer from "@/components/Footer";
 import EventCard from "@/components/EventCard";
 import { Button } from "@/components/ui/button";
 import { SEO } from "@/components/SEO";
-import hackathonPoster from "@/assets/events/temp.jpg";
-import roboWarsPoster from "@/assets/events/temp.jpg";
+
+
 import jengaPoster from "@/assets/events/Jenga.png";
 import bgmiPoster from "@/assets/events/bgmi.png";
 import codePoster from "@/assets/events/coder.png";
-import utkrishtPoster from "@/assets/events/temp.jpg";
+import roboPoster from "@/assets/events/Robo.png"
+import id from "@/assets/events/Investors.png"
+import rj from "@/assets/events/rj.png";
 
 
 
@@ -47,7 +49,8 @@ const Events = () => {
       "participants": "Solo participation (open to all departments)",
       "prize": "Exciting prizes for top performers",
       "description": "Get ready to turn up the volume at RJ Hunt – Utkrisht 2025! Step into the spotlight and let your voice do the magic in this thrilling hunt for the ultimate Radio Jockey. Participants will be judged on voice modulation, creativity, spontaneity, and on-air presence. It’s not just about what you say, but how you say it!",
-      poster: utkrishtPoster
+      poster: rj
+
     },
     {
       id: "cad-mad",
@@ -57,7 +60,7 @@ const Events = () => {
       participants: "Teams of 3",
       prize: "Exciting Rewards",
       description: " Showcase your AutoCAD skills by designing innovative structures and models.",
-      poster: hackathonPoster
+      
     },
     {
       "id": "finance-tambola",
@@ -67,7 +70,7 @@ const Events = () => {
       "participants": "Individual Participation (Open to all GSB Students)",
       "prize": "Attractive Prizes & Certificates.",
       "description": "An interactive financial literacy event that blends learning and fun through a pictorial Tambola game, helping students understand key concepts like savings, investment, taxation, and budgeting.",
-      poster: utkrishtPoster
+     
     },
 
     {
@@ -78,7 +81,7 @@ const Events = () => {
       "participants": "Team Participation (Open to all GSB Students)",
       "prize": "Prizes and Certificates for all",
       "description": "A fun and fast-paced branding challenge where teams identify brands, logos, and taglines flashed briefly on screen to test their marketing knowledge and brand recall.",
-      poster: utkrishtPoster
+       
     },
     {
       "id": "peoples-pulse",
@@ -88,7 +91,7 @@ const Events = () => {
       "participants": "Open to MBA/BBA Students (Teams of 2–3)",
       "prize": "Certificates & Prizes.",
       "description": "An HR-based interactive event with rounds like HR Quiz War, Case Crackers, and Ad Design Showdown — promoting strategic HR thinking, ethics, and creativity under the theme 'People at the Heart of Strategy'.",
-      poster: utkrishtPoster
+      
     },
     {
       "id": "techmanthan",
@@ -98,7 +101,7 @@ const Events = () => {
       "participants": "Individual or Team Participation",
       "prize": "Certificates & Recognition for Best Ideas",
       "description": "TechManthan – Ideas for Viksit Bharat empowers young minds to innovate for the nation’s technological growth. Participants act as youth representatives, presenting 2-minute speeches with ideas that can drive India’s transformation into a Viksit Bharat (Developed India). The event fosters leadership, innovation, and public speaking skills aligned with the Viksit Bharat@2047 vision.",
-      poster: utkrishtPoster
+       
     },
     {
       "id": "reel-o-tech",
@@ -108,7 +111,7 @@ const Events = () => {
       "participants": "Individual or Team (up to 3 members)",
       "prize": "Exciting Awards",
       "description": "Reel-O-Tech blends technology and creativity, allowing participants to craft short 60–90 second reels around tech life, innovation, or campus experiences. The event promotes storytelling, editing skills, and originality using tools like CapCut, Runway ML, and Synthesia, celebrating the spirit of Utkrisht 2025.",
-      poster: utkrishtPoster
+      
     },
     {
       "id": "techfusion",
@@ -118,7 +121,7 @@ const Events = () => {
       "participants": "Open to all(Team size: 1–4 members)",
       "prize": "Exciting Awards",
       "description": "A multidisciplinary competition that unites AI/ML, software development, and cybersecurity through three segments — Build, Predict, and Secure. Students innovate, analyze, and defend in real-world tech challenges designed to test creativity, technical acumen, and ethical problem-solving.",
-      poster: utkrishtPoster
+      
     },
     {
       "id": "anatomical-3d-model-making",
@@ -128,7 +131,7 @@ const Events = () => {
       "participants": "Pharmacy students of Tula’s Institute",
       "prize": "Certificates",
       "description": "An engaging hands-on activity organized by the Department of Pharmacy, Tula’s Institute of Pharmacy, where students designed 3D models of human anatomical structures. The activity aimed to strengthen understanding of anatomy, spatial visualization, and teamwork through creative model construction using diverse materials and techniques.",
-      poster: utkrishtPoster
+      
     },
     {
       "id": "soap-making-activity",
@@ -138,7 +141,7 @@ const Events = () => {
       "participants": "Pharmacy students of Tulas Institute",
       "prize": "Certificates",
       "description": "A hands-on workshop by the Department of Pharmacy, Tulas Institute, where students learned the process of saponification, formulation techniques, and cosmetic product preparation. Participants created customized soaps using natural oils and herbal additives, promoting creativity, eco-friendly innovation, and technical skills in cosmetic manufacturing.",
-      poster: utkrishtPoster
+       
     },
     {
       id: "robo-soccer",
@@ -148,7 +151,7 @@ const Events = () => {
       participants: "Teams of 4",
       prize: "TBA",
       description: "Design and program autonomous robot to play soccer against competing teams.",
-      poster: roboWarsPoster
+      
     },
     {
       id: "nutritive-garden",
@@ -160,7 +163,7 @@ const Events = () => {
       prize: "Certificates",
       description:
         " Learn the art of cultivating a nutritious kitchen garden with hands-on experience in planting, nurturing, and harvesting a variety of vegetables and herbs. Discover sustainable gardening practices that promote health and wellness.",
-      poster: utkrishtPoster
+     
     },
     {
       id: "robo-wars",
@@ -170,7 +173,7 @@ const Events = () => {
       participants: "Teams of 4",
       prize: "TBA",
       description: "Build and battle robots in an arena to outlast and outfight your opponents.",
-      poster: utkrishtPoster
+      poster: roboPoster
     },
     {
       id: "jenga-war",
@@ -190,7 +193,7 @@ const Events = () => {
       participants: "Team of 2",
       prize: "TBA",
       description: "Test your technical knowledge on essential electrical and electronics instruments in this exciting competition.",
-      poster: utkrishtPoster
+      
     },
     {
       id: "investors-den",
@@ -198,7 +201,7 @@ const Events = () => {
       category: "CENTERSTAGE",
       date: "November 13, 2025",
       participants: "Teams of 4",
-      poster: utkrishtPoster,
+      poster: id,
       prize: "TBA",
       description: "Investors Den is a Shark Tank-inspired entrepreneurship challenge."
     },
@@ -210,7 +213,7 @@ const Events = () => {
       participants: "Individual",
       prize: "Exciting Rewards",
       description: "Showcase innovative agricultural products and solutions that can revolutionize farming practices.",
-      poster: utkrishtPoster
+      
     },
     {
       id: "agri-quest",
@@ -220,7 +223,7 @@ const Events = () => {
       participants: "Individual",
       prize: "Exciting Rewards",
       description: "Test your knowledge of modern agricultural tools, quality seeds and seedling management in this fun quiz competition.",
-      poster: utkrishtPoster
+      
     },
     {
       id: "Mashroom",
@@ -230,7 +233,7 @@ const Events = () => {
       participants: "Individual",
       prize: "Certificates",
       description: " ",
-      poster: utkrishtPoster
+      
     },
     {
       id: "load-limit",
@@ -240,7 +243,7 @@ const Events = () => {
       participants: "Teams of 2",
       prize: "Exciting Rewards",
       description: "Test your structural engineering skills by designing and building a load-bearing model structure.",
-      poster: utkrishtPoster
+       
     },
     {
       id: "circuit-fab",
@@ -250,7 +253,7 @@ const Events = () => {
       participants: "Teams of 2",
       prize: "Exciting Rewards",
       description: "Design and fabricate a functional electronic circuit to meet specified performance criteria.",
-      poster: utkrishtPoster
+       
     },
     {
       id: "planting-competition",
@@ -260,7 +263,7 @@ const Events = () => {
       participants: "Team of 5",
       prize: "Certificates",
       description: " Engage in a hands-on planting competition to showcase your gardening skills and knowledge of plant care.",
-      poster: utkrishtPoster
+      
     },
 
     {
@@ -271,7 +274,7 @@ const Events = () => {
       participants: "Teams of 2",
       prize: "Exciting Rewards",
       description: "Navigate an electrifying maze using your knowledge of circuits and electronics.",
-      poster: utkrishtPoster
+       
     },
     {
       id: "unlocking-engine-secrets",
@@ -282,7 +285,7 @@ const Events = () => {
       participants: "Open to all undergraduate students",
       prize: "Certificates",
       description:" Learn the art of cultivating a nutritious kitchen garden with hands-on experience in planting, nurturing, and harvesting a variety of vegetables and herbs. Discover sustainable gardening practices that promote health and wellness.",
-      poster: utkrishtPoster
+       
     },
     {
       id: "doctor-driving",
@@ -293,7 +296,7 @@ const Events = () => {
       participants: "Open to all undergraduate students",
       prize: "Certificates",
       description:" Learn the art of cultivating a nutritious kitchen garden with hands-on experience in planting, nurturing, and harvesting a variety of vegetables and herbs. Discover sustainable gardening practices that promote health and wellness.",
-      poster: utkrishtPoster
+      
     },
     {
       id: "poster-presentation",
@@ -304,7 +307,7 @@ const Events = () => {
       participants: "Individual / Team of up to 3 members",
       prize: "Certificates & Recognition for Best Posters",
       description:"Showcase your innovative ideas and research insights through visually engaging posters! Present your creativity on themes like science, technology, innovation, and environment while demonstrating analytical thinking and clear communication skills.",
-      poster: utkrishtPoster,
+      
     },
     {
       id: "war-of-words",
@@ -316,7 +319,7 @@ const Events = () => {
       prize: "Certificates",
       description:
         "War of Words is an engaging inter-departmental Extempore competition designed to enhance communication, critical thinking, and analytical abilities of students. Participants will speak spontaneously on a given topic, showcasing their ability to think quickly, organize ideas effectively, and express viewpoints confidently. This event encourages clarity of thought, stage presence, and fluency — fostering leadership and intellectual confidence.",
-      poster: utkrishtPoster
+      
     },
     {
       id: "pottery-demonstration",
@@ -328,7 +331,7 @@ const Events = () => {
       prize: "Certificates for Participants",
       description:
         "A pottery demonstration showcasing the foundational techniques of creating a vessel on a potter's wheel. The session highlights the transformation of simple clay (earth and water) into a functional art piece through skill and heat (fire and air). This hands-on training offers an immersive experience for participants to learn forming processes and shaping techniques under expert guidance.",
-      poster: utkrishtPoster
+      
     },
     {
       id: "echo",
@@ -340,7 +343,7 @@ const Events = () => {
       prize: "Certificates for Participants",
       description:
         "Echoes of Talent 2.O is a vibrant cultural showcase that celebrates the creativity and passion of students. The event provides an open platform for participants to express themselves through singing, dancing, acting, stand-up comedy, and other performing arts. It aims to inspire confidence, highlight individuality, and create a joyful atmosphere of artistic expression and entertainment.",
-      poster: utkrishtPoster
+      
     },
     {
       id: "escape",
@@ -352,7 +355,7 @@ const Events = () => {
       prize: "Certificates for Participants",
       description:
         "The Escape Room 2.0  event is an intellectually stimulating and fun-filled competition designed to test participants’ problem-solving, analytical thinking, and technical abilities through a series of challenging phases. Each round progressively increases in complexity, encouraging logical reasoning, collaboration, and quick decision-making. Participants must clear each phase to unlock the next, ultimately aiming to “escape” by cracking the final challenge.",
-      poster: utkrishtPoster
+      
     },
     {
       id: "battle of thoughts",
@@ -364,7 +367,7 @@ const Events = () => {
       prize: "Certificates for Participants",
       description:
         "A Battle of Thoughts Competition is a formal event where participants speak for or against a topic, present logical arguments, counter opponents’ statements, and convince judges with strong reasoning, clarity, and confidence.It develops communication skills, critical thinking, leadership, and the ability to think under pressure.",
-      poster: utkrishtPoster
+      
     },
     {
       id: "the big",
@@ -376,7 +379,7 @@ const Events = () => {
       prize: "Exciting prizes.",
       description:
         "The Big Balloon show” is a creative and interactive event designed to celebrate imagination, teamwork, and artistic expression. In this activity, participants collaboratively create a vibrant tree using colorful balloons. The balloon tree serves as a symbolic representation of growth, unity, and the endless possibilities of rising higher, just as the name ‘the big balloon show’ signifies aspiration. The event encourages participants to think innovatively and work together to transform simple materials into a meaningful and visually appealing creation. It not only enhances creative thinking and coordination among team members but also promotes a sense of joy, positivity, and collective achievement. Through this fun-filled and inspiring activity, The Big balloon aims to nurture creativity, strengthen bonds, and remind everyone that with teamwork and imagination, we can all soar to new heights.",
-      poster: utkrishtPoster
+      
     },
     {
       id: "tradex",
@@ -388,7 +391,7 @@ const Events = () => {
       prize: "Cash prizes.",
       description:
         "Experience the Thrill of Real-Time Trading: The Live Intraday Trading Competition offers participants a thrilling real-time stock market experience using virtual capital and live market data. Conducted during the Tech Fest, the event tests analytical skills, risk management, and decision-making under pressure. Participants trade to maximize portfolio returns, gaining practical exposure to financial markets while competing for exciting prizes and recognition as top trading strategists.",
-      poster: utkrishtPoster
+      
     },
     {
       id: "auction",
@@ -400,7 +403,7 @@ const Events = () => {
       prize: "Exciting prizes.",
       description:
         "The Tula’s Auction League is an innovative and engaging sporting event that combines the thrill of cricket with the excitement of a live player auction. Inspired by professional tournaments like the Indian Premier League (IPL), it introduces students or local players to the dynamics of team building, bidding strategies, and competitive gameplay. Unlike traditional cricket tournaments where teams are pre-decided, the Auction League begins with a player auction, where team owners or captains purchase players using a fixed virtual budget.This process not only ensures fairness and balanced team formation but also adds entertainment and strategic depth to the competition.",
-      poster: utkrishtPoster
+       
     },
     {
       id: "turtle race",
@@ -412,7 +415,7 @@ const Events = () => {
       prize: "Exciting prizes.",
       description:
         "Turtle Race is one of the most exciting and entertaining events of Utkrisht, designed to test participants balance, control, and patience. Unlike traditional races where speed leads to victory, this unique challenge rewards those who can move slowly and steadily while maintaining perfect balance. The goal is simple yet tricky to be the last one to cross the finish line without losing control or touching the ground. This event is a blend of fun, focus, and physical coordination, giving participants an opportunity to showcase their composure and determination. It’s not just a race, it’s a test of endurance, concentration, and skill, all in the spirit of sportsmanship and enjoyment.",
-      poster: utkrishtPoster
+       
     },
     {
       "id": "reels-rumble",
@@ -422,7 +425,7 @@ const Events = () => {
       "participants": "Individual participation (open to all college students)",
       "prize": "Exciting prizes for most creative and impactful reels",
       "description": "Reels Rumble is a creative showdown where participants create a 45-second reel that captures the theme’s essence through storytelling, innovation, and cinematic execution. The event celebrates digital creativity and originality, challenging participants to craft a short yet impactful masterpiece.",
-      poster: utkrishtPoster
+      
     }
   ];
 
